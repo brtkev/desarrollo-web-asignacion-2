22e38f26db7c8649fca299d37c76b37d2471d2ce
brtkev <kbreto2911@gmail.com>
2025-03-18 22:28:19 -0400

    ejercicios DOM

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,62 @@
-export function setupCounter(element: HTMLButtonElement) {
+/** 2.1 setupCounter accede al DOM a travez de JS */
+export function setupCounter(element: HTMLButtonElement, container : HTMLElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
+  /** 2.3 agregamos un nuevo event listener para ordenar los libros */
+  element.addEventListener('click', () => sortBooks(container, counter));
   setCounter(0)
+}
+
+/**
+ * 2.3 Cambie el funcionamiento del botón Counter para que al presionarlo
+ordene los libros de manera alfabética por Titulo. Si lo presiona de
+nuevo, que lo ordene en orden descendente por Titulo.
+ * 
+  creo la funcion afuera para no interferir tanto con la funcion original
+ */
+
+  /** la funcion, se ejecuta guardando una lista de las cards y limpiando el contenedor original
+   * luego se sortea la lista de cards por titulo, ascendente o decendente
+   * y luego se rellena nuevamente el contenedor
+   */
+function sortBooks(container : HTMLElement, counter: number){
+  /** si le contador es par entonces, decendiente, si es impar ascendente */
+  const sortingDirection = counter % 2 === 0;
+
+  /** obtiene los book-card s, y duplica el objeto para que no se pierdan al borrarlos del elemento */
+  const bookCards = Array.from(container.children);
+  
+  /** borra los hijos del elemento */
+  // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
+  }
+
+  /** ordenamos las tarjetas */
+  const sortedBookCards = bookCards.sort( (bookA, bookB ) => {
+
+    /** obtenemos los titulos */
+    const titleA = bookA.getElementsByClassName("card-title")[0].textContent || ""; /** agrego "" para  */
+    const titleB = bookB.getElementsByClassName("card-title")[0].textContent || ""; /** evitar el error de TypeScript */
+    //https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+
+    // sorteamos decendente o ascendente
+    if(!sortingDirection){
+      return titleA.localeCompare(titleB);
+    }else {
+      return titleB.localeCompare(titleA);
+    }
+  })
+
+  /** agregamos de nuevo los libros a la lista */
+  sortedBookCards.forEach( bookCard => {
+    container.appendChild(bookCard)
+  })
+  
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
+/**
+ * 2.1 identifique en que partes del código se está haciendo uso de JavaScript
+  para acceder al DOM y qué hace cada instrucción que lo accede.
+ */
+
+
 import './style.css'
 import { setupCounter } from './counter.ts'
 import fantasyBooks from '../json/fantasy-books.json'
 
+/** 2.1 createBookCard accede al DOM a travez de JS, 
+ * crea elementos HTML, agrega clases a los elementos, 
+ * modifica textos y agrega hijos a elementos*/
 // Function to create a card for each book
-function createBookCard(book) {
+function createBookCard(book : any) { /** agrege : any para que deje de dar error TS */
+
+  /**
+   * 2.2 Agregue a cada tarjeta la foto del libro si posee foto. La foto debe estar a
+    la izquierda del nombre y autor del libro.
+   */
   const cardDiv = document.createElement('div');
   cardDiv.classList.add('card', 'mb-3');
 
   const cardBody = document.createElement('div');
   cardBody.classList.add('card-body');
+
+  /**
+   * creamos un div extra donde se mantendran el titulo y los autores
+   * ademas creamos la clase card-properties para darle estilos
+   */
+  const cardBodyProperties = document.createElement('div');
+  cardBodyProperties.classList.add('card-properties');
 
   const title = document.createElement('h5');
   title.classList.add('card-title');
@@ -18,23 +39,54 @@ function createBookCard(book) {
   author.classList.add('card-text');
   author.textContent = `Author: ${book.authors[0].name}`;
 
+  /**
+   * creamos un elemento imagen, en null por si el libro no trae imagen
+   */
+  let imagen = null;
+  if(book.formats){
+    imagen = document.createElement('img');
+
+    /**
+     * le agregamos la clase card-image para estilarlo y le damos los necesarios src y alt
+     */
+    imagen.classList.add('card-image');
+    imagen.src = book.formats['image/jpeg'];
+    imagen.alt = book.title +  " book cover";
+  }
+
   // Add other book properties as needed (e.g., subjects, formats, etc.)
 
-  cardBody.appendChild(title);
-  cardBody.appendChild(author);
+  /**
+   * agregamos la imagen si existe, de primero
+   */
+  if(imagen) cardBody.appendChild(imagen);
+  /**
+   * luego agregamos el titulo y el autor al div de propiedades
+   */
+  cardBodyProperties.appendChild(title);
+  cardBodyProperties.appendChild(author);
+  /** y por ultimo agregamos el div de propiedades al body */
+  cardBody.appendChild(cardBodyProperties);
   cardDiv.appendChild(cardBody);
 
   return cardDiv;
 }
 
+/**
+ * 2.1 bookContainer es accedido a travez del dom
+ */
 // Get the container where cards will be added
 const container = document.getElementById('bookContainer');
+if(container){ /** agrege validacion de existencia para que TS deje de dar error */
 
-// Loop through the books and create cards dynamically
-fantasyBooks.results.forEach((book) => {
-  const bookCard = createBookCard(book);
-  container.appendChild(bookCard);
-});
+  // Loop through the books and create cards dynamically
+  fantasyBooks.results.forEach((book) => {
+    const bookCard = createBookCard(book);
+    container.appendChild(bookCard);
+  });
+  
+  /** 2.1 setupCounter accede al DOM a travez de JS */
+  /** 2.3 adaptamos setupCounter para que reciba el contenedor de las cards y asi poder filtrarlas */
+  setupCounter(document.querySelector<HTMLButtonElement>('#counter')!, container);
+}
 
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
